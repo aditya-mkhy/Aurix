7ee1ea47ec7113ae48ba11b1d006a9850a4ac41f
aditya-mkhy <mahadevadityamukhiya@gmail.com>
2026-04-02 20:40:39 +0530

    feat: update ui

--- playlist_win.py
+++ playlist_win.py
@@ -269,7 +269,7 @@ class SongRow(QWidget):
         self.menu.addAction("Play next")
         self.menu.addAction("Add to queue")
         self.menu.addSeparator()
-        self.menu.addAction("Remove from Playlist")
+        self.menu.addAction("Remove song")
         self.menu.setStyleSheet("""
             QMenu#SearchMenu {
                 background-color: transparent; 
